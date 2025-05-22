fb4bcba740785295f930084f77cc85b32fa4e32d
Mykola Dymytrenko <dymytrenko73@icloud.com>
2025-05-22 21:44:59 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Form } from './components/Form/Form'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bulma/css/bulma.min.css';
+// import './Form.css';
+
+
+function App() {
+  return (
+    <div className="section">
+      <div className="container">
+        <Form />
+      </div>
+    </div>
+
+  )
+}
+
+export default App
