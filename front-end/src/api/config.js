67e4bb655b6de6538d8555b45cd0ebdca9accc73
Mykola Dymytrenko <dymytrenko73@icloud.com>
2025-08-12 16:40:52 +0200

    add console.log for debug

--- front-end/src/api/config.js
+++ front-end/src/api/config.js
@@ -1,6 +1,6 @@
 export const API_BASE = import.meta.env.DEV ? '/api' : import.meta.env.VITE_API_BASE;
 
-  // console.log('DEV=', import.meta.env.DEV, 'API_BASE=', API_BASE);
+console.log('API_BASE =', import.meta.env.VITE_API_BASE);
 
 export const getAuthHeaders = () => ({
   Accept: 'application/json',
